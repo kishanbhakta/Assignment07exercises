adee436a875b2ef23195b185d6cd8205b6cb2cfe
Kishan Bhakta <kishanbhakta@Kishans-MacBook-Pro.local>
2015-05-17 18:57:22 -0400

    completed 1-3, and 5

--- main.js
+++ main.js
@@ -2,25 +2,44 @@
 // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
 // ---------------------
 
-function max(){
-    //...
-}
+function max(num1, num2){
+
+    if (num1 > num2) {
+        return num1 ;
+    }
+    else if (num2 > num1) {
+        return num2 ;
+    }
+};
+
 
 // ---------------------
 // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
 // ---------------------
 
-function maxOfThree(){
-    //...
-}
+function maxOfThree(num1, num2, num3){
+
+    if (num1 > num2 && num1 > num3) {
+        return num1 + " is largest";
+    }
+    else if (num2 > num1 && num2 > num3) {
+        return num2 + " is largest";
+    }
+    else if (num3 > num1 && num3 > num2) {
+        return num3  + " is largest";
+    }
+};
 
 // ---------------------
 // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
 // ---------------------
 
-function isVowel(char){
-    //...
-}
+function isVowel(char) {
+   if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+       return true;
+   } else
+       return false;
+};
 
 // ---------------------
 // Write a function translate() that will translate a text into "rÃ¶varsprÃ¥ket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
@@ -34,13 +53,19 @@ function rovarspraket(phrase){
 // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
 // ---------------------
 
-function sum(){
-    //...
+function sum(numArray){
+    var answer = numArray.reduce(function(a,b) {
+        return a + b;
+    })
+return answer;
 }
 
-function multiply(){
-    //...
-}
+function multiply(numArray2){
+    var answer = numArray2.reduce(function(a,b) {
+        return a * b;
+    })
+ return answer;
+};
 
 // ---------------------
 // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
